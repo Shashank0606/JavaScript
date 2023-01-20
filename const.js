a6ac3ab084d4bd1b0663c5daa59254e1f3e8b777
Shashank0606 <shashankrathore606@gmail.com>
2023-01-20 21:42:18 +0530

    [Shashank]Add: Basics Functions

---
+++ const.js
@@ -0,0 +1,17 @@
+// define MY_FAV as a constant and give it the value 7
+const MY_FAV = 7;
+
+// throwerror
+MY_FAV = 20;
+
+// MY_FAV is 7
+console.log("my favorite number is: " + MY_FAV);
+
+// trying to redeclare a constant throws an error
+const MY_FAV = 20;
+
+// throw error
+var MY_FAV = 20;
+
+//throws error
+let MY_FAV = 20;
---
+++ datatype.js
@@ -0,0 +1,20 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Blue";
+let lastName = "Rathore";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = { firstName: "Shashank", lastName: "Rathore" };
+
+// Array object:
+const cars = ["Audi", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
